ab88d01d9d75729690111c9a871a775ad9e9f065
hygl <3154803225@qq.com>
2025-10-28 18:00:50 +0800

    update avatar

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/demo-avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Wok",
 	bio: "目前是一名大学生，热爱编程，也喜欢记录思考。",
 	links: [
